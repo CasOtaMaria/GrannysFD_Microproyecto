3b5400daef3ee7868a64fa4c737c26bf071e7c7b
BERRY\casti <castillootamendimaria@gmail.com>
2025-02-27 22:00:03 +0100

    Script movimento de personaje y prueba.

---
+++ Assets/Scripts/Movement_Character.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Movement_Character : MonoBehaviour
+{
+    public float velocidad;
+    public Rigidbody rbCharacter;
+
+    private Vector2 inputMov; //El movimiento del personaje sera 2D (ejes x,z)
+
+    private void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        inputMov.x = Input.GetAxis("Horizontal"); //Accede al Input System Package donde los inputs de las teclas ya están definidos
+        inputMov.y = Input.GetAxis("Vertical");
+        inputMov.Normalize(); //Para que sea un vector unitario (que no aumente la velocidad al moverse en diagonal)
+
+        rbCharacter.linearVelocity = new Vector3(inputMov.x*velocidad, 0f, inputMov.y*velocidad); //0f porque no quiero que salte en ningún momento
+    }
+}
+    
