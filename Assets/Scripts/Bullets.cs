d2a1b48f41580c5de2ceb2e8f847eee56331a16e
BERRY\casti <castillootamendimaria@gmail.com>
2025-03-04 15:29:04 +0100

    Scripts: daño al enemigo arreglado. Revisar: las balas no terminan de destruirse bien.

--- Assets/Scripts/Bullets.cs
+++ Assets/Scripts/Bullets.cs
@@ -5,18 +5,30 @@ public class Bullets : MonoBehaviour
     public float damage = 5;
     public EnemyPatrol_Health enemyPatrolHealth;
    
-    void Awake()
-    {
-        
-    }
-
     void OnCollisionEnter(Collision collision)
     {
-        
+        if (!collision.gameObject.CompareTag("Player")) //para evitar que la bala se destruya nada mas disparar
+        {
+            if (collision.gameObject.CompareTag("Enemy"))
+            {
+                //accedo al script de salud (antes no lo cogia al colisionar)
+                EnemyPatrol_Health enemyHealth = collision.gameObject.GetComponent<EnemyPatrol_Health>();
+
+                if (enemyHealth != null) //para asegurar que encuentra el script 
+                {
+                    enemyHealth.TakeDamage(damage);
+                    Debug.Log("Salud restante: " + enemyHealth.health);
+                }
+                //Destroy(gameObject);
+            }
+            Destroy(gameObject);
+        }
+
+        /* //CODIGO ANTIGUO
         if (collision.gameObject.tag == "Enemy")
         {
             Destroy(gameObject);
             enemyPatrolHealth.TakeDamage(damage);     
-        }        
+        }*/       
     }
 }
--- Assets/Scripts/EnemyPatrol_Health.cs
+++ Assets/Scripts/EnemyPatrol_Health.cs
@@ -16,12 +16,25 @@ public class EnemyPatrol_Health : MonoBehaviour
     
     public void TakeDamage(float damageTaken)
     {
+        if (health > 0)
+        {
+            health -= damageTaken;
+            health = Mathf.Max(health, 0);  //hago que la salud sea positiva (porque antes solo me salia neg)
+            Debug.Log("Salud restante: " + health);
+        }
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            enemyPatrol.CreateCoin();
+        }
+
+        /* CODIGO ANTIGUO
         health =- damageTaken;
         if (health <= 0)
         {
             //enemyPatrol.CreateCoin();
             Destroy(gameObject);
-        }
+        }*/
     }
 
 
